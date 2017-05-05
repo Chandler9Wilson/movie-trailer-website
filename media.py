eb4cf796b9acf644b5346bb4d8ae346b3246c8c9
Chandler Wilson <chandler9wilson@gmail.com>
2017-05-05 13:02:18 -0500

    updated to docstring in class init

--- media.py
+++ media.py
@@ -5,7 +5,7 @@ import webbrowser
 class Movie():
 
     def __init__(self, info):
-        # assigns the basic info of the movie to the class instance
+        """Assigns the basic info of the movie to the class instance"""
         self.title = info["movie_title"]
         self.poster_image_url = info["poster_image_url"]
         self.trailer_youtube_url = info["trailer_youtube_url"]
