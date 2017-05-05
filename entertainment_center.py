95ec70736c240465a1a458dd674231f15d055bfb
Chandler Wilson <chandler9wilson@gmail.com>
2017-05-05 09:31:02 -0500

    updated comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,12 +5,14 @@ import fresh_tomatoes
 movies = {}
 
 with open('movie_data.json') as data_file:
+    # imports json file
     data = json.load(data_file)
 
 
 def data_to_movie(movie_info):
-    # instatiates Movie then appends to movies list
+    # loops over movies dictionary after import from json
     for movie in movie_info['movies']:
+        # instatiates Movie then adds to the  movies dictionary
         movie_title = movie["movie_title"]
         movies[movie_title] = media.Movie(movie)
 
